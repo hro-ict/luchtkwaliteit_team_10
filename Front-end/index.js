3294103481bfbe2e5cb989cf94edcb582ec8c222
NoodleSomRequiem <semvanveen@outlook.com>
2023-05-21 14:20:46 +0200

    fixed buttons, alleen nog linken aan back end

--- Front-end/index.js
+++ Front-end/index.js
@@ -1,5 +1,19 @@
 
+//HTML
 const display = document.getElementById("display");
 const testbtn = document.querySelector("#test");
 
 testbtn.addEventListener("click", () => loadSavedModel() && console.log("Loading saved model.."));
+
+//moet nog wat in de fetch
+function loadSavedModel() {
+    fetch("...")
+        .then((response) => response.json())
+        .then((model) => modelLoaded(model))
+}
+
+if (luchtkwaliteit == 1) {
+    display.innerText = `luchtkwaliteti is goed`
+} else {
+    display.innerText = `luchtkwaliteit is slecht`
+}
